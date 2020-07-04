164bb0590b8c366499f6a81eb39b09f8788a2d82
R4ph3uz <r4ph3uz@gmail.com>
2020-07-04 16:42:42 +0200

    fond
    maj du fd en rose
    essai d'un mxw800px en blanc

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title>Document</title>
+    <title>MsP</title>
     
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,8 +9,22 @@
     <link rel="stylesheet" href="./private/css/style.css">
 </head>
 <body>
-    
+    <header>
+    <div class="header__logo">
+        <img src="./private/img/logos/" alt="logo">
+    </div>
+    <ul>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+    </ul>
+    </header>
 
+    <div class="mxw800p">
+
+
+    </div>
     
 </body>
 </html>
