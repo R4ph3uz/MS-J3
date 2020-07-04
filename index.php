6a409acc85bf61f65660a9edd6224ebc70c82b7a
R4ph3uz <r4ph3uz@gmail.com>
2020-07-04 18:10:21 +0200

    banner- début

--- index.php
+++ index.php
@@ -1,30 +1,76 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <title>MsP</title>
-    
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="./private/css/style.css">
 </head>
+
 <body>
     <header>
-    <div class="header__logo">
-        <img src="./private/img/logos/" alt="logo">
-    </div>
-    <ul>
-        <li><a href=""></a></li>
-        <li><a href=""></a></li>
-        <li><a href=""></a></li>
-        <li><a href=""></a></li>
-    </ul>
+        <div class="header__logo">
+            <img src="./private/img/logos/" alt="logo">
+        </div>
+        <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
     </header>
 
-    <div class="mxw800p">
+    <div class="mxw900p">
+        <section class="banniere">
+            <h1 class="hidden"> Mary's sweatest place</h1>
+            <div class="banniere__container">
+                <div class="banniere__slider">
+                    <div class="slide">
+                        <div class="assiette"> SVG + <img src="./private/img/carroussel/plat1.webp" alt="plat 1"> </div>
+                        <div class="content">
+                            <h2><img src="./private/logos/ecriture/" alt=""></h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione rem nostrum ducimus laborum omnis a recusandae esse, perspiciatis adipisci atque blanditiis odio minus, laudantium quam, aliquid voluptates! Dignissimos, cumque iure.</p>
+                            <a href="">Call to action</a>
+                        </div>
+                    </div>
 
+                    <div class="slide">
+                        <div class="assiette"> SVG + <img src="./private/img/carroussel/plat1.webp" alt="plat 1"> </div>
+                        <div class="content">
+                            <h2><img src="./private/logos/ecriture/" alt=""></h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione rem nostrum ducimus laborum omnis a recusandae esse, perspiciatis adipisci atque blanditiis odio minus, laudantium quam, aliquid voluptates! Dignissimos, cumque iure.</p>
+                            <a href="">Call to action</a>
+                        </div>
+                    </div>
+
+                    <div class="slide">
+                        <div class="assiette"> SVG + <img src="./private/img/carroussel/plat1.webp" alt="plat 1"> </div>
+                        <div class="content">
+                            <h2><img src="./private/logos/ecriture/" alt=""></h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione rem nostrum ducimus laborum omnis a recusandae esse, perspiciatis adipisci atque blanditiis odio minus, laudantium quam, aliquid voluptates! Dignissimos, cumque iure.</p>
+                            <a href="">Call to action</a>
+                        </div>
+                    </div>
+
+                    <div class="slide">
+                        <div class="assiette"> SVG + <img src="" alt=""> </div>
+                        <div class="content"> </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="bannierre__arrows">
+                <a href="">L Key</a>
+                <a href="">D Key</a>
+                <a href="">R Key</a>
+            </div>
+        </section>
 
     </div>
-    
+
 </body>
+
 </html>
