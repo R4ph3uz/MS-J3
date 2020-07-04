65f315cebdc4b7c6a854aea14ba5936482a2e332
R4ph3uz <r4ph3uz@gmail.com>
2020-07-04 17:12:44 +0200

    header color + spacing

--- index.php
+++ index.php
@@ -1,30 +1,37 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <title>MsP</title>
-    
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="./private/css/style.css">
 </head>
+
 <body>
     <header>
-    <div class="header__logo">
-        <img src="./private/img/logos/" alt="logo">
-    </div>
-    <ul>
-        <li><a href=""></a></li>
-        <li><a href=""></a></li>
-        <li><a href=""></a></li>
-        <li><a href=""></a></li>
-    </ul>
+        <div class="header__container">
+            <div class="header__logo">
+                <img src="./private/img/logos/" alt="logo">
+            </div>
+            <ul>
+                <li><a href="">Accueil</a></li>
+                <li><a href="">A propos</a></li>
+                <li><a href="">Services</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </div>
     </header>
 
     <div class="mxw800p">
-
+    <section>
+        bla bla bla
+    </section>
 
     </div>
-    
+
 </body>
+
 </html>
